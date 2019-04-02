8d07646ff14f2fe6865ed1d345adcc37e28f4a69
PANDYA21 <bhaumik.pandya21@gmail.com>
2019-04-02 12:44:14 +0200

    clean up, wrapper index.js

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const addon = require('./build/Release/addon');
 const _ = require('lodash');
 
 module.exports = function mine(transactions, antecedent) {
-  const associations = addon.mine(transactions, antecedent);
+  let transactionsWithAntecedent = [];
+  _.each(transactions, x => {
+    if (x.indexOf(antecedent) !== -1) {
+      transactionsWithAntecedent.push(x);
+    }
+  })
+  const associations = addon.mine(transactionsWithAntecedent, antecedent);
   const ans = _.sortBy(associations, x => -x.nAB);
   return ans;
 };
