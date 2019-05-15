c368a692b48f9691310c791af9255cc69372c3eb
PANDYA21 <bhaumik.pandya21@gmail.com>
2019-05-15 17:56:44 +0200

    parallel another approach

--- parallel/new.js
+++ parallel/new.js
@@ -28,70 +28,30 @@ function serial() {
   return t2 - t1;
 }
 
+const nCores = 5;
 function parallel() {
   const { execFile } = require('child_process');
   const t1 = Date.now();
+  const len = 45000;
 
-  execFile('node', ['./worker.js', 'START=0', 'END=9000'], (error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(stdout);
-    console.error(stderr);
-    if (stdout.indexOf('done') !== -1) {
-      parallelDone(t1, Date.now());
-    }
-  });
-
-  execFile('node', ['./worker.js', 'START=9001', 'END=18000'], (error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(stdout);
-    console.error(stderr);
-    if (stdout.indexOf('done') !== -1) {
-      parallelDone(t1, Date.now());
-    }
-  });
-
-  execFile('node', ['./worker.js', 'START=18001', 'END=27000'], (error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(stdout);
-    console.error(stderr);
-    if (stdout.indexOf('done') !== -1) {
-      parallelDone(t1, Date.now());
-    }
-  });
-
-  execFile('node', ['./worker.js', 'START=27001', 'END=36000'], (error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(stdout);
-    console.error(stderr);
-    if (stdout.indexOf('done') !== -1) {
-      parallelDone(t1, Date.now());
-    }
-  });
-
-  execFile('node', ['./worker.js', 'START=36001', 'END=45000'], (error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(stdout);
-    console.error(stderr);
-    if (stdout.indexOf('done') !== -1) {
-      parallelDone(t1, Date.now());
-    }
-  });
+  for (let i = 0; i < nCores; i++) {
+    execFile('node', ['./worker.js', 'START=' + (len / nCores * i + 1), 'END=' + (len / nCores * (i + 1))], (error, stdout, stderr) => {
+      if (error) {
+        console.error(error);
+      }
+      console.log(stdout);
+      console.error(stderr);
+      if (stdout.indexOf('done') !== -1) {
+        parallelDone(t1, Date.now());
+      }
+    });
+  }
 }
 
 let pars = [];
 function parallelDone(t1, t2) {
   pars.push(t2 - t1);
-  if (pars.length === 5) {
+  if (pars.length === nCores) {
     console.log('\n\n\n---------------------------------------');
     console.log({ ser, par: _.max(pars), ratio: ser / _.max(pars) });
   }
